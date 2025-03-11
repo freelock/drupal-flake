cd29b61084dd6c735fa36b89c4801c43d90f4734
John Locke <john@freelock.com>
2025-03-10 17:19:58 -0700

    feat(template): add a default template to install this flake locally
    Use `nix flake init -t github:freelock/drupal-flake` to install this flake as template in an existing (or new) project

--- README.md
+++ README.md
@@ -36,11 +36,23 @@ On my machine with warmed caches, this takes about 2 1/2 minutes to download Dru
 
 In the shell you run this in, the "init" task will print the admin username and password.
 
-## Run demo locally
+## Install locally
 
-You can run this locally by cloning this repo, and then simply `nix run .#demo`.
+You can run this locally by installing the template:
+
+```
+nix flake init -t github:freelock/drupal-flake
+nix run .#demo
+```
+This will install a fresh copy of Drupal CMS if you don't have one, and launch it all in one go!
 
 The demo setup is skipped if the web/index.php file already exists.
+
+If you already have a flake.nix and you want to use ours instead, you can add --refresh to the init command to overwrite the files in your local directory from templates/ in this repo:
+
+```
+nix flake init -t github:freelock/drupal-flake --refresh
+```
 
 ## Setting name, port, domain
 
@@ -49,11 +61,35 @@ You can now set the base project name, port, domain, PHP version, and Drupal pac
 ```
 PROJECT_NAME=myproject PORT=9901 DRUPAL_PACKAGE=drupal/recommended-project:^10.4 nix run --impure .#demo
 ```
-.env support for these variables is planned, but not yet working.
+
+You can set these variables in a .env file - but this is subject to how Nix Flakes handle files -
+if you are in a git directory, the .env file needs to be added to git or else Nix will ignore it entirely.
+
+Copy the .env.example to .env and edit as desired.
+
+### Note on domain, port
+
+To be able to resolve your site to localhost, you may need to add an entry to your /etc/hosts file. The DDev project has created a wildcard DNS that allows anything that ends in .ddev.site to resolve to localhost. So by default the domain is set to the PROJECT_NAME.ddev.site, which should always work.
+
+Ports must be higher than 1024, or else you need some sort of root access to be able to use them.
 
 
-## Coming soon
+## Next up
 
-2. Make a template
+The Demo target installs a clean new copy of Drupal CMS -- but what about existing projects?
 
-This will allow you to use `nix init --flake github:freelock/drupal-flake#cms` to create a new project with Drupal CMS ready to install, or `nix init --flake github:freelock/drupal-flake` to install the dev stack in an existing Drupal project.
+`nix run` starts up all the servers, and assumes a document root of /web. Coming soon:
+
+1. Make webroot configurable
+2. Fix existing settings.php to load settings.nix.php
+3. Make `drush sqlc` work -- currently erroring out due to no TTY
+4. Add other supporting tools to the devShell -- scripts to hook things up, mysql/mariadb, etc
+4. Fix Nginx's extra listen on port 8080 that prevents multiple instances from running
+5. SSL certs - possibly switch to Caddy
+6. Helper to run on standard ports (80, 443)
+7. Additional services -
+  - Solr
+  - PhpMyAdmin
+  - Mailpit
+  - Redis
+8. Build - Create a Docker image, VM, or Nix Package
--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "PHP flake for Drupal development";
+  description = "Template for Drupal development environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -9,200 +9,35 @@
     services-flake.url = "github:juspay/services-flake";
   };
 
-  outputs = inputs:
+  outputs = inputs @ { self, nixpkgs, flake-parts, systems, process-compose-flake, services-flake, ... }:
+  let
+    templateFlake = import ./template/flake.nix;
+    templateOutputs = templateFlake.outputs inputs;
 
-    inputs.flake-parts.lib.mkFlake {
-      inherit inputs;
-    } {
-      systems = import inputs.systems;
-      imports = [
-        inputs.process-compose-flake.flakeModule
-      ];
+  in
+  {
+    templates.default = {
+      path = ./template;
+      description = "Development environment for Drupal with Nix";
+      welcomeText = ''
+        # Drupal Development Environment
 
-      perSystem = { self', pkgs, config, lib, ...}:
-        let
-          # Function to read env vars with defaults
-          getEnvWithDefault = name: default:
-            let
-              envValue = builtins.getEnv name;
-              envFileStr = builtins.readFile (toString ./.env);
-              envFile = builtins.tryEval (
-                if builtins.pathExists ./.env
-                then builtins.listToAttrs (
-                  builtins.map (line:
-                    let pair = builtins.match "([^=]+)=(.*)" line;
-                    in if pair == null then null
-                      else { name = builtins.head pair; value = builtins.elemAt pair 1; }
-                  ) (builtins.filter (line: line != "" && !(lib.hasPrefix "#" line))
-                    (lib.splitString "\n" envFileStr))
-                )
-                else {}
-              );
-              envVars = if envFile.success then envFile.value
-                else {};
-            in
-            if envValue != ""
-              then envValue
-              else if builtins.hasAttr name envVars
-                then envVars.${name}
-                else default;
+        Your new Drupal environment has been created!
 
-          # Configuration with environment fallbacks
-          projectName = getEnvWithDefault "PROJECT_NAME" "drupal-demo";
-          domain = getEnvWithDefault "DOMAIN" "${projectName}.ddev.site";
-          port = getEnvWithDefault "PORT" "8088";
-          phpVersion = getEnvWithDefault "PHP_VERSION" "php83";
-          drupalPackage = getEnvWithDefault "DRUPAL_PACKAGE" "drupal/cms";
+        To get started:
+        1. Edit .env to configure your project and add to git
+        2. Run `direnv allow`
+        3. Run `nix run` to start the environment
+        4. Add .direnv, data and other environment-related dirs/files to .gitignore
 
-
-          inherit (inputs.services-flake.lib) multiService;
-
-
-          baseConfig ={
-            imports = [
-              inputs.services-flake.processComposeModules.default
-              # (multiService ./.services/caddy.nix)
-              (multiService ./.services/phpfpm.nix)
-              (multiService ./.services/init.nix)
-            ];
-
-            services.mysql."${projectName}-db" = {
-              enable = true;
-              settings.mysqld = {
-                # Optional: Set other MySQL settings
-                # port = 3307;
-                # bind_address = "127.0.0.1";
-                skip_grant_tables = true;
-                skip_networking = true;
-              };
-              initialDatabases = [
-                {
-                  name = "drupal"; # Database name
-                }
-              ];
-
-            };
-            services.phpfpm."${projectName}-php" = {
-              enable = true;
-              # Override PHP version:
-              phpVersion = phpVersion;
-            };
-            # Create log dir
-            settings.processes.setupNginx = {
-              command = ''
-                mkdir -p logs
-              '';
-            };
-            services.nginx."${projectName}-nginx" = {
-              enable = true;
-              # Override domain:
-              httpConfig = ''
-                server {
-                  listen ${port};
-                  server_name ${domain};
-                  root web;
-                  index index.php index.html index.htm;
-
-                  # logging
-                  access_log logs/${projectName}-access.log;
-                  error_log logs/${projectName}-error.log;
-
-                  location / {
-                    try_files $uri $uri/ /index.php?$query_string;
-                  }
-
-                  location ~ \.php$ {
-                    fastcgi_split_path_info ^(.+\.php)(/.+)$;
-                    fastcgi_pass unix:/tmp/${projectName}-php.sock;
-                    fastcgi_index index.php;
-                    include ${pkgs.nginx}/conf/fastcgi_params;
-
-                    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-                    fastcgi_param PATH_INFO $fastcgi_path_info;
-                    fastcgi_param QUERY_STRING $query_string;
-                    fastcgi_intercept_errors on;
-                  }
-
-                  # Deny access to . files
-                  location ~ /\. {
-                    deny all;
-                  }
-
-                  # Allow fpm ping and status
-                  location ~ ^/(fpm-status|fpm-ping)$ {
-                    access_log off;
-                    allow 127.0.0.1;
-                    deny all;
-                    fastcgi_pass unix:data/php/php-fpm.sock;
-                    include ${pkgs.nginx}/conf/fastcgi_params;
-                  }
-
-                }
-              '';
-            };
-            #services.caddy."${projectName}" = {
-            #  enable = true;
-              # Override domain:
-            #  settings.domain = domain;
-            #};
-
-
-            # Open browser to the domain
-            settings.processes.open-browser = {
-              command = ''
-                sleep 2
-                xdg-open http://${domain}:${port}
-              '';
-              depends_on."${projectName}-nginx".condition = "process_healthy";
-            };
-          };
-
-      in
-      {
-        process-compose."default" = { config, ...}: baseConfig;
-
-        # New demo target to install Drupal
-        process-compose."demo" = { config, ...}:
-          lib.recursiveUpdate baseConfig {
-            services.init."cms" = {
-              enable = true;
-              projectName = projectName;
-              drupalPackage = drupalPackage;
-              #php = baseConfig.services.phpfpm."${projectName}-php".settings.php;
-              # mysqlDataDir = mysqlDataDir; # /. + "/data/${projectName}-db";
-              # php = baseConfig.settings.processes."${projectName}-php".default;
-            };
-            settings.processes.cms = {
-
-
-              readiness_probe = {
-                # Check if Drupal is installed
-                exec.command = "test -f web/index.php";
-                initial_delay_seconds = 1;
-                period_seconds = 3;
-                timeout_seconds = 1;
-                success_threshold = 1;
-                failure_threshold = 60;
-              };
-              availability = {
-                restart = "no";
-              };
-            };
-
-            # Make other services depend on the Drupal installation
-            settings.processes."${projectName}-php".depends_on.cms.condition = "process_completed_successfully";
-            settings.processes."${projectName}-nginx".depends_on.cms.condition = "process_completed_successfully";
-          };
-
-
-        # Dev shell for debugging
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [
-            config.process-compose."default".services.outputs.devShell
-
-          ];
-
-        };
-      };
+        If you want to update this template, run `nix flake init -t github:freelock/drupal-flake --refresh`. This will overwrite existing files with the latest version of the template.
+      '';
     };
-  }
+
+    # Re-export the template's outputs to maintain direct usability
+    inherit (templateOutputs)
+      packages
+      apps
+      devShells;
+  };
+}
