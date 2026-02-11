69509ce135619b542d95c0b4dd9f202c205532ac
John Locke <john@freelock.com>
2026-02-11 15:40:24 -0800

    feat: #3573002 error: `php84Packages.phpstan` has been removed, use `phpstan` instead.

--- template/.services/php-fpm.nix
+++ template/.services/php-fpm.nix
@@ -51,7 +51,7 @@ let
     else [
       php
       php.packages.composer
-      php.packages.phpstan
+      phpstan
       # codesniffer package is broken at the moment
       # php.packages.php-codesniffer
       finalPkgs.phpunit
